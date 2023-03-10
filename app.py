aa6d6e0cc07dba545383a6bd87626751cceb9e77
Lloyd McKinney <42.lloyd.mckinney@gmail.com>
2023-03-10 19:39:43 +0000

    update data cleaning and dashboard

--- app.py
+++ app.py
@@ -16,16 +16,21 @@ with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
-@st.cache_data
+@st.experimental_memo
 def get_data_from_excel(path: str):
     return pd.read_excel(path)
 
 
 df = get_data_from_excel('data/data_clean.xlsx')
-# df_gb = get_data_from_excel('data/data_groupby.xlsx')
 
 # SIDEBAR
 st.sidebar.header("Filters")
+
+highest_product = st.sidebar.multiselect(
+    "Select the product level:",
+    options=df['Highest Product'].unique(),
+    default=df['Highest Product'].unique()
+)
 
 csm_status = st.sidebar.multiselect(
     "Select the CSM Status:",
@@ -39,14 +44,6 @@ region = st.sidebar.multiselect(
     default=df.Region.unique()
 )
 
-st.sidebar.selectbox('Select data', ('q2', 'q3'))
-
-highest_product = st.sidebar.multiselect(
-    "Select the product level:",
-    options=df['Highest Product'].unique(),
-    default=df['Highest Product'].unique()
-)
-
 df_selection = df.query(
     "`CSM Status Stage` == @csm_status & Region == @region & `Highest Product` == @highest_product"
 )
@@ -57,21 +54,20 @@ Created with ❤️ by Anna.
 ''')
 
 # MAIN PAGE
-# ---------------------------- Row A
 fontsize = 50
 valign = "left"
 sline = "CX Onboarding"
 lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
 
-htmlstr = f"""<p style='background-color: rgb(154,216,225); 
-                        color: rgb(0,0,0, 0.75); 
-                        font-size: {fontsize}px; 
-                        border-radius: 7px; 
-                        padding-left: 12px; 
-                        padding-top: 18px; 
-                        padding-bottom: 18px; 
+htmlstr = f"""<p style='background-color: rgb(154,216,225);
+                        color: rgb(0,0,0, 0.75);
+                        font-size: {fontsize}px;
+                        border-radius: 7px;
+                        padding-left: 12px;
+                        padding-top: 18px;
+                        padding-bottom: 18px;
                         line-height:25px;'>
-                        </style><BR><span style='font-size: {fontsize}px; 
+                        </style><BR><span style='font-size: {fontsize}px;
                         margin-top: 0;'>{sline}</style></span></p>"""
 
 st.markdown(lnk + htmlstr, unsafe_allow_html=True)
@@ -83,54 +79,51 @@ Users who have logged in
 col1, col2, col3, col4, col5, col6 = st.columns(6)
 
 # Daily
-daily_users = df_selection[df_selection['Last Product usage date'].notna()].groupby('Date')['Account ID'].nunique().reset_index()
-today = daily_users.max()['Date']
-dau_today = int(daily_users[daily_users['Date'] == today]['Account ID'])
+daily_users = df_selection[df_selection['date_usage'].notna()].groupby('date_usage')['Account ID'].nunique().reset_index()
+today = daily_users.max()['date_usage']
+dau_today = int(daily_users[daily_users['date_usage'] == today]['Account ID'])
 
 # Yesterday
 yesterday = today - pd.Timedelta(days=1)
-dau_yesterday = int(daily_users[daily_users['Date'] == yesterday]['Account ID'])
+dau_yesterday = int(daily_users[daily_users['date_usage'] == yesterday]['Account ID'])
 before_yesterday = yesterday - pd.Timedelta(days=1)
-before_yesterday = int(daily_users[daily_users['Date'] == before_yesterday]['Account ID'])
+dau_before_yesterday = int(daily_users[daily_users['date_usage'] == before_yesterday]['Account ID'])
 
-# Week
-last_week = today - pd.Timedelta(days=7)
-previous_week = last_week - pd.Timedelta(days=7)
-dau_week = int(daily_users[daily_users['Date'] == last_week]['Account ID'])
-previous_week = int(daily_users[daily_users['Date'] == previous_week]['Account ID'])
+# Weekly
+week = df_selection.copy()
+week['date_w'] = pd.to_datetime(week['date_usage']) - pd.to_timedelta(7, unit='d')
+week = week.groupby([pd.Grouper(key='date_w', freq='W-MON')])['Account ID'].nunique().reset_index().sort_values('date_w')[['date_w', 'Account ID']]
+current_week = week.iloc[-1]['date_w']
+dau_week = int(week.iloc[-1]['Account ID'])
+wau_previous = int(week.iloc[-2]['Account ID'])
 
-# Month
-last_month = today - pd.Timedelta(days=30)
-previous_month = last_month - pd.Timedelta(days=30)
-dau_month = int(daily_users[daily_users['Date'] == last_month]['Account ID'])
-previous_month = int(daily_users[daily_users['Date'] == previous_month]['Account ID'])
+# Monthly
+month = df_selection.copy()
+month['date_usage'] = pd.to_datetime(month['date_usage'])
+month = month.groupby(month.date_usage.dt.month)['Account ID'].nunique().reset_index().sort_values('date_usage')
+mau = int(month.iloc[-1]['Account ID'])
+mau_previous = int(month.iloc[-2]['Account ID'])
 
 dau_average = round(daily_users['Account ID'].mean(), 1)
 
+stickiness = (dau_today/mau) * 100
+
 with col1:
     st.metric("DAU (today) ", dau_today, f"{round((dau_today - dau_yesterday)/dau_yesterday * 100, 1)}% vs yesterday ({dau_yesterday})")
-col2.metric("DAU (yesterday)", dau_yesterday, f"{round((dau_yesterday - before_yesterday)/before_yesterday * 100, 1)}% vs day before ({before_yesterday})")
-col3.metric("WAU (last 7 days)", dau_week, f"{round((dau_week - previous_week)/previous_week * 100, 1)}% vs week before ({previous_week})")
-col4.metric("MAU (last 30 days)", dau_month, f"{round((dau_month - previous_month)/previous_month * 100, 1)}% vs month before ({previous_month})")
-col5.metric("DAU (average)", dau_average)
-col6.metric("Stickiness (DAU/MAU)", f"{round(dau_today/dau_month, 2)*100}%")
+    col2.metric("DAU (yesterday)", dau_yesterday, f"{round((dau_yesterday - dau_before_yesterday)/dau_before_yesterday * 100, 1)}% vs day before ({dau_before_yesterday})")
+    col3.metric("WAU (last 7 days)", dau_week, f"{round((dau_week - wau_previous)/wau_previous * 100, 1)}% vs week before ({wau_previous})")
+    col4.metric("MAU (last 30 days)", mau, f"{round((mau - mau_previous)/mau_previous * 100, 1)}% vs month before ({mau_previous})")
+    col5.metric("DAU (average)", dau_average)
+    col6.metric("Stickiness (DAU/MAU)", f"{round(stickiness, 2)}%")
 
-fig_growth = px.line(daily_users, x='Date', y='Account ID', title="<b>Daily Active Users</b>")
+fig_growth = px.line(daily_users, x='date_usage', y='Account ID', title="<b>Daily Active Users</b>")
 fig_growth.update_layout(plot_bgcolor="rgba(0,0,0,0)", xaxis=(dict(showgrid=False)))
 fig_growth.update_yaxes(rangemode="tozero")
 st.plotly_chart(fig_growth, use_container_width=True)
 
-#---------------------------- ROW B
 # CHURN ANALYTICS
 c1, c2, c3 = st.columns(3)
 with c1:
-    # st.markdown('### Churn')
-    # by_csm = df.groupby('CSM Status Stage').count()['Account ID'].reset_index()
-    # churned_total = int(by_csm[by_csm['CSM Status Stage'] == 'Churned']['Account ID'])
-    # no_churned_total = by_csm[by_csm['CSM Status Stage'] != 'Churned']['Account ID'].sum()
-    # churn_data = pd.DataFrame({'churn': [churned_total], 'not_churn': [no_churned_total]}).T.reset_index()
-    # fig_pie = px.pie(churn_data, names='index', values=0)
-    # st.plotly_chart(fig_pie, use_container_width=True)
     st.markdown('### Churn by region')
     churn_by_region = df_selection[df_selection['CSM Status Stage'] == 'Churned'].groupby(['Region']).count()[
         'Account ID'].reset_index().rename(columns={'Account ID': 'total_users_churned'})
@@ -146,8 +139,6 @@ with c1:
         go.Scatter(x=churn_by_region['Region'], y=churn_by_region['total_users'], name="Total users", mode="lines"),
         secondary_y=True)
     fig_churn_by_region.update_xaxes(title_text="Region")
-
-    # Set y-axes titles
     fig_churn_by_region.update_yaxes(title_text="Churn", secondary_y=False)
     fig_churn_by_region.update_yaxes(title_text="Total users", secondary_y=True)
     st.plotly_chart(fig_churn_by_region, use_container_width=True)
@@ -159,7 +150,7 @@ with c2:
     st.plotly_chart(fig_churn_by_product, use_container_width=True)
 with c3:
     st.markdown('### Churn by number of delivery partners')
-    churn_by_partners = df[df['CSM Status Stage'] == 'Churned'].groupby(['# delivery partners']).count()[
+    churn_by_partners = df_selection[df_selection['CSM Status Stage'] == 'Churned'].groupby(['# delivery partners']).count()[
         'Account ID'].reset_index()
     fig_churn_by_partners = px.bar(churn_by_partners, x='# delivery partners', y='Account ID')
     st.plotly_chart(fig_churn_by_partners, use_container_width=True)
@@ -167,42 +158,21 @@ with c3:
 #---------------------------- ROW C
 col1, col2 = st.columns(2)
 with col1:
-    st.markdown("""### Users that have logged in""")
+    st.markdown("""### Logged-in users by CSM status""")
     logged_in = df_selection.groupby(['CSM Status Stage', 'has_logged_in']).count().sort_values(['Account ID', 'CSM Status Stage'], ascending=False).reset_index()
     fig = px.bar(logged_in, x="CSM Status Stage", y='Account ID', color="has_logged_in", barmode='group')
     st.plotly_chart(fig, use_container_width=True)
 with col2:
-    st.markdown("""### Users that have logged in""")
-    logged_in = df_selection.groupby(['CSM Status Stage', 'has_logged_in']).count().sort_values(['Account ID', 'CSM Status Stage'], ascending=False).reset_index()
-    fig = px.bar(logged_in, x="CSM Status Stage", y='Account ID', color="has_logged_in", barmode='group')
-    st.plotly_chart(fig, use_container_width=True)
+    st.markdown('### Users by product')
+    df_selection = df_selection[df_selection['Region'] != 'Unknown']
+    gb = df_selection.groupby(['Region', 'Highest Product']).nunique().reset_index()
+    fig_growth = px.bar(gb, x='Region', y='Account ID', color='Highest Product', barmode='group')
+    fig_growth.update_layout(plot_bgcolor="rgba(0,0,0,0)", xaxis=(dict(showgrid=False)))
+    fig_growth.update_yaxes(rangemode="tozero")
 
-# ACTIVE BY REGION
-# st.markdown('### Active by region')
-# product_by_region = df_selection.groupby("Region").count()[['CSM Status Stage']].sort_values('CSM Status Stage',
-#                                                                                              ascending=False)
-# fig_product_by_region = px.bar(
-#     product_by_region, x='CSM Status Stage', y=product_by_region.index,
-#     title="<b>Product by region</b>",
-#     # color_discrete_sequence=["#0083B8"] * len(product_by_region)
-# )
-# fig_product_by_region.update_layout(plot_bgcolor="rgba(0,0,0,0)", xaxis=(dict(showgrid=False)))
-# st.plotly_chart(fig_product_by_region)
-#
-# # Row C
-# fig_growth = px.line(
-#     df_gb, x='year_month', y='growth',
-#     title="<b>Growth rate</b>",
-#     # color_discrete_sequence=["#0083B8"] * len(product_by_region)
-# )
-# fig_growth.update_layout(
-#     plot_bgcolor="rgba(0,0,0,0)",
-#     xaxis=(dict(showgrid=False))
-# )
-#
-# fig_growth.update_yaxes(rangemode="tozero")
-#
-# st.plotly_chart(
-#     fig_growth,
-#     # use_container_width=True
-# )
+    st.plotly_chart(fig_growth, use_container_width=True)
+
+st.markdown('''
+---
+Created with ❤️ by Anna.
+''')
